e39ee472dd9c7726483c4ebdeb6aa46eff4dc797
Bacon <99620849+onlinebacon@users.noreply.github.com>
2023-11-07 10:04:50 -0300

    Added method "shoot" to sphere-math.js

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,19 +1,23 @@
 # Changelog
 
+## [0.2.0] - 2023-11-07
+### Added
+- method `shoot` in `sphere-math.js`
+
+### Changed
+- method `azimuth` renamed to `calcAzimuth` in `sphere-math.js`
+
 ## [0.1.0] - 2023-11-07
 ### Added
-
 - file `vec3.js`
 - file `sphere-math.js`
 
 ## [0.0.1] - 2023-11-06
 ### Added
-
 - file `package.json` for node.js module execution
 - file `CHANGELOG.md`
 - file `README.md`
 
 ## [0.0.0] - 2023-11-06
 ### Added
-
-- trig.js
+- file `trig.js`
--- sphere-math.js
+++ sphere-math.js
@@ -23,11 +23,23 @@ export const vec3ToLatLon = ([ x, y, z ]) => {
 	return [ lat, lon ];
 };
 
-export const azimuth = ([ lat1, lon1 ], [ lat2, lon2 ]) => {
+export const calcAzimuth = ([ lat1, lon1 ], [ lat2, lon2 ]) => {
 	const dtLon = lon2 - lon1;
 	const x = cos(dtLon)*cos(lat2);
 	const y = sin(dtLon)*cos(lat2);
 	const z = sin(lat2);
 	const nz = z*cos(lat1) - x*sin(lat1);
 	return uAngle(nz, y);
+};
+
+export const shoot = ([ lat, lon ], azimuth, distance) => {
+	const sinR = sin(distance);
+	const vec = new Vec3(
+		cos(distance),
+		sinR*sin(azimuth),
+		sinR*cos(azimuth),
+	);
+	vec.rotY(lat, vec);
+	vec.rotZ(-lon, vec);
+	return vec3ToLatLon(vec);
 };
